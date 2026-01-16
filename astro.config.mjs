15ddd30fb43063b8d0b30198d405448a205d4bbe
Cristian Hernandez <1278819+clystian@users.noreply.github.com>
2026-01-16 00:16:24 -0500

    ci: Add GitHub Actions workflow to deploy Astro site to GitHub Pages and configure static output.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://clystian.github.io',
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   }
